f6e5cd4ff3de9a777f520ad8ebe0a7b9c1c66e5f
Jason Riha <superjar30@gmail.com>
2020-10-09 10:17:35 -0500

    created intitial program from HW7 GitHub

---
+++ src/Driver.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Driver 
+{
+	public static void main(String[] args)
+	{
+		BibleBookCollection theNewTestament = new BibleBookCollection("data.dat");
+		try
+		{
+			BibleBook answer = theNewTestament.getFirstBookGivenSearchTerm("Jesus");
+			answer.display();
+		}
+		catch(Exception e)
+		{
+			System.out.println("No Book Found");
+		}
+		
+	}
+}
